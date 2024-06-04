a7c1418747651e3978110be34ed87bde24541502
Cemre <cemreefe@gmail.com>
2024-06-04 14:11:02 +0100

    Update and rename .user_defined to user_defined.sh

--- user_defined.sh
+++ user_defined.sh
@@ -1,14 +1,17 @@
                                                                                                                               
-## Play beep sound.
+## Play beep sound
 alias beep="tput bel"
 
-## Start chromium-based browser with CORS disabled (i.e. ```google-chrome $NOCORS```).
+## Start chromium-based browser with CORS disabled (i.e. ```google-chrome $NOCORS```)
 export NOCORS="--args --profile-directory=Default --disable-web-security"
 
-## Function that executes command until it succeeds.
+## Function that executes command until it succeeds
 function wfor {
   until "$@"; do
     echo "Command failed, retrying in 3 seconds..."
     sleep 3
   done
 }
+
+## Add currently staged files to last local commit
+alias gitup="git commit --amend --no-edit"
