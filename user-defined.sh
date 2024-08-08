8b53882b81765f7241ca17fe3007af4abb8fa899
Cemre <cemreefe@gmail.com>
2024-08-08 12:43:31 +0100

    Update user-defined.sh - Add pullrebase

--- user-defined.sh
+++ user-defined.sh
@@ -1,11 +1,11 @@
                                                                                                                               
-## Play beep sound
+## Play beep sound.
 alias beep="tput bel"
 
-## Start chromium-based browser with CORS disabled (i.e. ```google-chrome $NOCORS```)
+## Start chromium-based browser with CORS disabled (i.e. ```google-chrome $NOCORS```).
 export NOCORS="--args --profile-directory=Default --disable-web-security"
 
-## Function that executes command until it succeeds
+## Function that executes command until it succeeds.
 function wfor {
   until "$@"; do
     echo "Command failed, retrying in 3 seconds..."
@@ -13,7 +13,7 @@ function wfor {
   done
 }
 
-## Add currently staged files to last local commit
+## Add currently staged files to last local commit.
 alias gitup="git commit --amend --no-edit"
 
 ## Update ```user-defined.sh```
@@ -23,4 +23,12 @@ function zupdate {
     git pull origin main
     source user-defined.sh
     cd "$current_dir"
+}
+
+## Pull latest on target branch and rebase current branch on target branch.
+function pullrebase() {
+    local initial_branch=$(git symbolic-ref --short HEAD)
+    local target_branch=$1
+
+    git checkout $target_branch && git pull && git checkout $initial_branch && git rebase origin/$target_branch
 }
